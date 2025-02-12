21f5c46c6e61e654049c1c0dfddbf8ff1bb53f92
Jango-7 <jhoanngo2@gmail.com>
2025-02-11 22:01:00 -0300

    Contador de palabras

---
+++ contador-letras.py
@@ -0,0 +1,11 @@
+# 1. Solicitar al user que ingrese una palabra.
+
+palabra_ingresada = input("Ingresa una palabra: ")
+
+# 2. Contamos la cantidad de letras que tiene la palabra ingresada.
+
+cantidad_letras = len(palabra_ingresada)
+
+# 3. Mostramos cuantas letras tiene dicha palabra.
+
+print(f'La palabra ingresada: {palabra_ingresada} tiene {cantidad_letras} letras.')
